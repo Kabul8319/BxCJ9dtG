e9dd14353b635c7f18be0dfcd8c93618b8e5f028
kabul Maluda <kabul.maluda.gmail.com>
2015-06-12 11:48:03 -0700

    my APp

---
+++ myapp/src/main/java/de/kabul/myapp/App.java
@@ -0,0 +1,13 @@
+package de.kabul.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
